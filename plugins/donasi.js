1d11eb378a9059b79a99a0b420aa67bf1fabf4a8
kenzostore <90239401+kenzostore@users.noreply.github.com>
2022-03-18 14:19:48 +0700

    Update donasi.js
    .MMgzhsh

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -6,7 +6,7 @@ let handler = async m => m.reply(`
 
 ╭─「 Donasi • Non Pulsa 」
 │ • https://saweria.co/
-│ • https://saweria.co/ariffb
+│ • https://saweria.co/
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi','donate']
